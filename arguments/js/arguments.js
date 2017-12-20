f0a5b38aa0c9f492a135dc1cafa0969d94e9dbfa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 12:23:54 -0500

    3 - Valores por 'default' en argumentos: Tratamiento de argumentos (Destructuring, JSON Objects) indefinido en ES6

--- arguments/js/arguments.js
+++ arguments/js/arguments.js
@@ -1,10 +1,17 @@
 /* Valores por defecto en argumentos en ES6 */
-function pareja( ella = 'María', el ) {
-  return [ ella, el ];
+
+/* Usando el Destructuring (asignando valores con respecto a un arreglo) */
+function sumar(
+  [ num1, num2 ] = [ 1, 2 ],      // Destructuring
+  { num3: num3 } = { num3: 3 }    // JSON Object
+) {
+
+  return num1 + num2 + num3 ;
 }
 
-console .group( 'Parejas' );
-  console .log( pareja() );                         // [ "María", undefined ]
-  console .log( pareja( 'Luisa' ) );                // [ "Luisa", undefined ]
-  console .log( pareja( 'Ana Maria', 'Pedro' ) );   // [ "Ana María", "Pedro" ]
+/* Resultados */
+console .group( 'Destructuring' );
+  console .log( sumar() );                          // 6
+  console .log( sumar( [ 3, 5 ] ) );                // 11
+  console .log( sumar( [ 3, 5 ], { num3:2 } ) );    // 10
 console .groupEnd();
