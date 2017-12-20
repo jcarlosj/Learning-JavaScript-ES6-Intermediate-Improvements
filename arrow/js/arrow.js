a6871d7ca6f511c67a26c7173e3d0e67c2953692
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 09:54:36 -0500

    1 - Arrow Functions: Función Flecha en ES6 (dos o más parámetros)

--- arrow/js/arrow.js
+++ arrow/js/arrow.js
@@ -1,4 +1,4 @@
 /* Función Flecha en ES6 */
-var saludar = saludo => saludo;     /* 'saludo' es parámetro y valor de retorno */
+var saludar = ( saludo, nombre ) => saludo + ' ' + nombre;  /* 'saludo' y 'nombre' son parámetros y valor de retorno */
 
-console .log( saludar( 'Hola!' ) );
+console .log( saludar( 'Hola!', 'Juan' ) );
