0157f605098ceab21e115e8cef4cb83ea46f6579
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 12:50:21 -0500

    3 - Valores por 'default' en argumentos: Tratamiento de argumentos como cadenas de texto indefinido en ES6 (Cambio de color de fondo)

--- arguments/js/arguments.js
+++ arguments/js/arguments.js
@@ -1,17 +1,11 @@
 /* Valores por defecto en argumentos en ES6 */
-
-/* Usando el Destructuring (asignando valores con respecto a un arreglo) */
-function sumar(
-  [ num1, num2 ] = [ 1, 2 ],      // Destructuring
-  { num3: num3 } = { num3: 3 }    // JSON Object
-) {
-
-  return num1 + num2 + num3 ;
+function cambiarColorFondo( elemento, color = 'yellow' ) {
+  var cuadro = document .getElementById( elemento );
+  cuadro .style .backgroundColor = color;
 }
 
-/* Resultados */
-console .group( 'Destructuring' );
-  console .log( sumar() );                          // 6
-  console .log( sumar( [ 3, 5 ] ) );                // 11
-  console .log( sumar( [ 3, 5 ], { num3:2 } ) );    // 10
-console .groupEnd();
+window .onload = function() {
+  cambiarColorFondo( 'cuadro-1' );              // Color de fondo amarillo
+  cambiarColorFondo( 'cuadro-2', 'blue' );      // Color de fondo azul
+  cambiarColorFondo( 'cuadro-3', undefined );   // Color de fondo amarillo
+}
