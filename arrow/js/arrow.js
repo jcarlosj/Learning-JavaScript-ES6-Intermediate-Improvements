384e4380781eeefe9b2550f6183ff2316524c0a7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 10:02:28 -0500

    1 - Arrow Functions: Función Flecha en ES6 (con llaves)

--- arrow/js/arrow.js
+++ arrow/js/arrow.js
@@ -1,5 +1,8 @@
 /* Función Flecha en ES6 */
-setTimeout(
-  () => alert( 'Hola desde una función anónima en ES6' ),   /* Función Flecha anónima */
-  3000                                                      /* Se ejecutará a los 3 Segundos  */
-);
+var saludar = () => {
+  saludo = 'Hola';
+  nombre = 'Juan';
+  alert( saludo + ' ' + nombre );
+}
+
+saludar();
