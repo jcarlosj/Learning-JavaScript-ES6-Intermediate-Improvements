c028f7a1c492b15fce57d03df8f8f2c040702a35
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 11:13:39 -0500

    3 - Valores por 'default' en argumentos: Tratamiento de varios argumentos numéricos indefinidos en ES6

--- arguments/js/arguments.js
+++ arguments/js/arguments.js
@@ -1,6 +1,9 @@
 /* Valores por defecto en argumentos en ES6*/
-function multiplicar( numero1, numero2 = 1 ) {
-  return numero1 * numero2;
+function sumar( arg1, arg2 = 4, arg3 = 3 ) {
+  return arg1 + arg2 + arg3;
 }
 
-console .log( multiplicar( 5 ) );   // 5
+console .group( 'Resultados' );
+  console .log( 'Suma: ', sumar( 2 ) );
+  console .log( 'Es igual a 9?: ', sumar( 2 ) === 9 );
+console .groupEnd();
