af8fb9140c3c0184bd1f15fb0276e56c8359576f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 11:58:06 -0500

    3 - Valores por 'default' en argumentos: Tratamiento de función como argumentos indefinido en ES6 pasado a otra función

--- arguments/js/arguments.js
+++ arguments/js/arguments.js
@@ -1,21 +1,25 @@
-/* Valores por defecto en argumentos en ES6*/
-function agregar( valor, arreglo = [] ) {
-  arreglo .push( valor );
-  return arreglo;
+/* Valores por defecto en argumentos en ES6 */
+
+/* Función como parámetro de otra */
+function saludos( cosa = predeterminada() ) {
+  return cosa;
 }
 
-frutas = [];
+/* Funciones corrientes */
+function predeterminada() {
+  return 'Hola!';
+}
+function ingles() {
+  return 'Hello!';
+}
+function personalizado( nombre ) {
+  return 'Hola ' + nombre + ' saludos desde ES6';
+}
 
-/* Pasa solo un valor como parámetro (Crea un array por valor) */
-console .group( 'Valores sin pasar array' );
-  console .log( agregar( 7 ) );     // [ 7 ]
-  console .log( agregar( 1 ) );     // [ 1 ]
-  console .log( agregar( 9 ) );     // [ 9 ]
-console .groupEnd();
-
-/* Pasa solo un valor y un array como parámetros (Agrega el valor al array que se le pasa como parámetro ) */
-console .group( 'Array de frutas' );
-  console .log( agregar( 'Manzana', frutas ) );   // [ "Manzana" ]
-  console .log( agregar( 'Mango', frutas ) );     // [ "Manzana", "Mango" ]
-  console .log( agregar( 'Mora', frutas ) );      // [ "Manzana", "Mango", "Mora" ]
+/* Resultados */
+console .group( 'Saludos ' );
+  console .log( 'Corriente ', saludos() );                                // Hola
+  console .log( 'En inglés ', saludos( ingles() ) );                      // Hello
+  console .log( 'Personalizado ', saludos( personalizado( 'Juan' ) ) );   // Hola Juan saludos desde ES6
+  console .log( 'Repite ', saludos( 'Juanito' ) );                        // Juanito
 console .groupEnd();
