b81714e0107323ad57763b9a0b07cfe5ebf26984
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-20 11:03:51 -0500

    3 - Valores por 'default' en argumentos: Tratamiento de valores indefinidos (undefined) en ES6

--- arguments/js/arguments.js
+++ arguments/js/arguments.js
@@ -1,6 +1,5 @@
-/* Valores por defecto en argumentos en ES5 o JavaScript Tradicional */
-function multiplicar( numero1, numero2 ) {
-  numero2 = typeof numero2 !== 'undefined' ? numero2 : 1;   // Operador ternario
+/* Valores por defecto en argumentos en ES6*/
+function multiplicar( numero1, numero2 = 1 ) {
   return numero1 * numero2;
 }
 
