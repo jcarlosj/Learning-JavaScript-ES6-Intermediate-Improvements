4a4e684501507baca737697bf7e23bcfc974350e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-21 11:32:07 -0500

    6 - Symbol Type: Obtener las variables o constantes de tipo 'Symbol' contenidas en una variable de tipo 'Object'

--- symbol-type/js/symbol-type.js
+++ symbol-type/js/symbol-type.js
@@ -1,32 +1,83 @@
 /* Valores por defecto en argumentos en ES6 */
 function symbolType() {
 
-  // Declara constantes de tipo "Symbol"
+  /* Declara constantes de tipo "Symbol" */
   const primera = Symbol();
   const segunda = Symbol();
+  /* Declara variable de tipo Objeto Vacio */
+  let variableObjeto = {};      // Equivale a:   variableObjeto = new Object();
 
-  console .group( 'Validaciones de variables tipo "Symbol"' );
+  /* Asigna las constantes "Symbol" al Objeto usando la notación de [] no la de  . */
+  variableObjeto[ primera ] = 'PRIMERA';
+  variableObjeto[ segunda ] = 'SEGUNDA';
+
+  /* Valores contenidos en los objetos */
+  console .group( 'Valores contenidos en los objetos (primera, segunda)' );
     console .log(
-      'Symbol( "foo" ) !== Symbol( "foo" ) ... ',
-      Symbol( "foo" ) !== Symbol( "foo" )
+      ' >>> variableObjeto[ primera ] ... ',
+      variableObjeto[ primera ]
     );
     console .log(
-      'primera !== segunda ... ',
-      primera !== segunda
-    );       // Equivalente a la validación anterior
+      ' >>> variableObjeto[ segunda ] ... ',
+      variableObjeto[ segunda ]
+    );
+  console .groupEnd();
 
-    /* Valida los tipos */
-    console .group( 'Constantes de tipo "Symbol"' );
-      console .log(
-        'typeof primera !== "symbol" ... ',
-        typeof primera !== "symbol"
-      );
-      console .log(
-        'typeof segunda !== "symbol" ... ',
-        typeof segunda !== 'symbol'
-      );
-    console .groupEnd();
+  /* Validaciones */
+  console .group( 'JSON .stringify' );
+    console .log(
+      ' >>> JSON .stringify( variableObjeto ) ... ',
+      JSON .stringify( variableObjeto )
+    );
+    console .log(
+      'JSON .stringify( variableObjeto[ primera ] ) ... ',
+      JSON .stringify( variableObjeto[ primera ] )
+    );
+    console .log(
+      'JSON .stringify( variableObjeto[ segunda ] ) ... ',
+      JSON .stringify( variableObjeto[ segunda ] )
+    );
+  console .groupEnd();
+
+  console .group( 'Object .keys' );
+    console .log(
+      ' >>> Object .keys( variableObjeto ) ... ',
+      Object .keys( variableObjeto )
+    );
+  console .groupEnd();
+
+  console .group( 'Object .getOwnPropertyNames' );
+    console .log(
+      '  >>> Object .getOwnPropertyNames( variableObjeto ) ... ',
+      Object .getOwnPropertyNames( variableObjeto )
+    );
+    console .log(
+      'Object .getOwnPropertyNames( variableObjeto[ primera ] ) ... ',
+      Object .getOwnPropertyNames( variableObjeto[ primera ] )
+    );
+    console .log(
+      'Object .getOwnPropertyNames( variableObjeto[ segunda ] ) ... ',
+      Object .getOwnPropertyNames( variableObjeto[ segunda ] )
+    );
+  console .groupEnd();
+
+  /* OBTENER las VARIABLES o CONSTANTES de tipo SYMBOL contenidos en un OBJETO*/
+  console .group( 'Object .getOwnPropertySymbols' );
+    console .log(
+      ' >>> Object .getOwnPropertySymbols( variableObjeto ) ... ',
+      Object .getOwnPropertySymbols( variableObjeto )
+    );
+    console .log(
+      'Object .getOwnPropertySymbols( variableObjeto[ primera ] ) ... ',
+      Object .getOwnPropertySymbols( variableObjeto[ primera ] )
+    );
+    console .log(
+      'Object .getOwnPropertySymbols( variableObjeto[ segunda ] ) ... ',
+      Object .getOwnPropertySymbols( variableObjeto[ segunda ] )
+    );
   console .groupEnd();
 }
+
+
 
 symbolType();
