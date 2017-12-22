370d7bb7405aaedc8057118141ef744ca004e686
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-22 13:31:32 -0500

    10 - Objects: Método assign()

--- objects/js/objects.js
+++ objects/js/objects.js
@@ -1,14 +1,15 @@
 /* Valores por defecto en argumentos en ES6 */
 
 // Crea Objeto forma ES6
-var Persona = {
-  nombre: 'Juan',
-  [ 'id' + aleatorio() ]: 4      // Nombre de Variable de forma Aleatoria
-};
+var objeto1 = { a: 1 };
+var objeto2 = { b: 2 };
+var objeto3 = { c: 3 };
 
-function aleatorio() {
-  return Math .floor( Math .random() * 10 );
-}
-
-// Resultados
-console .log( Persona );
+// Método assign() para copiar objetos
+var objetoFinal = Object .assign( objeto1, objeto2, objeto3 );   // objectoFinal y objeto1 se ven modificados los otros no
+console .group( 'Objetos: Método assign()' );
+  console .log( 'objetoFinal ', objetoFinal );     // { a: 1, b: 2, c: 3 }
+  console .log( 'objeto1 ',  objeto1 );    // { a: 1, b: 2, c: 3 }
+  console .log( 'objeto2 ',  objeto2 );    // { b: 2 }
+  console .log( 'objeto3 ',  objeto3 );    // { c: 3 }
+console .groupEnd();
