97a011d98961147d0c7a91750df788cedffdc8f0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-22 13:34:34 -0500

    10 - Objects: Método assign() 2do Ejemplo
    Aclaración con esté método pasamos los valores de un objeto a otro
    a diferencia de cuando asignamos un objeto a otro estamos pasando
    referencias, ejemplo
    
    var obj1 = { saludo: 'Hola' };
    var obj2 = {}
    
    obj1 = obj2;

--- objects/js/objects.js
+++ objects/js/objects.js
@@ -1,15 +1,15 @@
 /* Valores por defecto en argumentos en ES6 */
 
 // Crea Objeto forma ES6
-var objeto1 = { a: 1 };
-var objeto2 = { b: 2 };
+var objeto1 = { a: 1, b: 1, c: 1 };
+var objeto2 = { b: 2, c: 2 };
 var objeto3 = { c: 3 };
 
 // Método assign() para copiar objetos
-var objetoFinal = Object .assign( objeto1, objeto2, objeto3 );   // objectoFinal y objeto1 se ven modificados los otros no
+var objetoFinal = Object .assign( {}, objeto1, objeto2, objeto3 );   // objectoFinal se modifica los otros no
 console .group( 'Objetos: Método assign()' );
   console .log( 'objetoFinal ', objetoFinal );     // { a: 1, b: 2, c: 3 }
-  console .log( 'objeto1 ',  objeto1 );    // { a: 1, b: 2, c: 3 }
-  console .log( 'objeto2 ',  objeto2 );    // { b: 2 }
+  console .log( 'objeto1 ',  objeto1 );    // { a: 1, b: 1, c: 1 }
+  console .log( 'objeto2 ',  objeto2 );    // { b: 2, c: 2 }
   console .log( 'objeto3 ',  objeto3 );    // { c: 3 }
 console .groupEnd();
