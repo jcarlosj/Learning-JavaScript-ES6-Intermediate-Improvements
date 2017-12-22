a87861612c19cdbe1ed17e836a28fd703018e549
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-22 13:03:20 -0500

    10 - Objects: Definición tradicional de funciones

--- objects/js/objects.js
+++ objects/js/objects.js
@@ -1,21 +1,33 @@
 /* Valores por defecto en argumentos en ES6 */
 
 // Crea Objeto forma tradicional
-var mujer = {
-  nombres: 'Luisa',
-  apellidos: 'Escalante'
-};
-// Crea Objeto con sintáxis corta ES6
-var nombres = 'Jorge',
-    apellidos = 'Rodríguez';
+var operaciones = {
+  // Dentro del objeto de forma explicita
+  sumar :function( num1, num2 ) {
+    return num1 + num2;
+  },
+  restar :function( num1, num2 ) {
+    return num1 + num2;
+  },
+}
 
-var hombre = {
-  nombres,
-  apellidos
+// Fuera del objeto
+operaciones .multiplicar = function ( num1, num2 ) {
+  return num1 * num2;
+};
+operaciones .dividir = function ( num1, num2 ) {
+  return num1 / num2;
 };
 
 // Resultados
-console .group( 'Objetos: Sintáxis Corta' );
-  console .log ( 'Tradicional: ', mujer .nombres + ' ' + mujer .apellidos );
-  console .log ( 'Sintáxis Corta ES6: ', hombre .nombres + ' ' + hombre .apellidos );
+var numero1 = 12,
+    numero2 = 2;
+console .log( 'numero 1: ' + numero1 );
+console .log( 'numero 2: ' + numero2 );
+
+console .group( 'Objetos: Definición tradicional de funciones' );
+  console .log ( 'Suma: ', operaciones .sumar( numero1, numero2 ) );
+  console .log ( 'Restar: ', operaciones .restar( numero1, numero2 ) );
+  console .log ( 'Multiplicar: ', operaciones .multiplicar( numero1, numero2 ) );
+  console .log ( 'Dividir: ', operaciones .dividir( numero1, numero2 ) );
 console .groupEnd();
