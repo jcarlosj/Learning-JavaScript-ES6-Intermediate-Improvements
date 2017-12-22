d7ec88685147e113e73b25e7c42059a9cc877e0a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-22 11:39:07 -0500

    9 - Destructuring: Extraer valores de un Objeto

--- destructuring/js/destructuring.js
+++ destructuring/js/destructuring.js
@@ -1,15 +1,22 @@
 /* Valores por defecto en argumentos en ES6 */
 
-// Crea un array de datos
-let datos = [ 'Renault Clio', 'Manzana', 'Nevera', 40 ];
+// Crea un objeto de datos
+let modulo = {
+  calcularCuadrado( longitud ) {
+    console .log( 'Cuadrado de longitud ' + longitud + ' es: ', longitud * longitud );
+  },
+  perimetroCirculo( radio ) {
+    console .log( 'Perímetro de un circulo de radio ' + radio + ' es: ', 2 * Math .PI * radio );
+  },
+  saludar( nombre ) {
+    console .log( 'Hola ' + nombre + ' esto es Destructuring' );
+  }
+};
 
-// Extraemos los datos del Array 'datos' usando el destructuring asignandolo a una lista de variables
-let [ carro, fruta, electrodomestico, meses ] = datos;
+// Extraemos los datos del Objeto 'datos' usando el destructuring asignandolo a una lista de variables
+let { calcularCuadrado, saludar, perimetroCirculo } = modulo;     // Aunque esten invertidos si se llaman igual los resultados se extraen
 
 // Imprimimos los datos extraidos
-console .group( 'Destructuring' );
-  console .log( 'carro: ', carro );
-  console .log( 'fruta: ', fruta );
-  console .log( 'electrodomestico: ', electrodomestico );
-  console .log( 'mes: ', meses );
-console .groupEnd();
+calcularCuadrado( 5 );
+perimetroCirculo( 2 );
+saludar( 'Juan' );
