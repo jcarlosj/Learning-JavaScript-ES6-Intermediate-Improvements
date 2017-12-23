086e496d2f7e8b162744ef187f94f67631ce2b65
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 12:01:55 -0500

    12 - Propagación o Spread: Spread entrer Arrays

--- spread/js/spread.js
+++ spread/js/spread.js
@@ -1,8 +1,14 @@
 /* Objetos de propagación o Spread (Expansión) en ES6 */
 
-function saludar( nombre, apellido, edad ) {
-  alert( 'Hola ' + nombre + ' ' + apellido + ' que cumplas feliz tus ' + edad + ' años' );
-}
+var numeros1 = [ 0, 1, 2 ],
+    numeros2 = [ 3, 4, 5 ];
 
-var persona = [ 'Laura', 'Fonseca', 29 ];           // Array
-saludar( ...persona );                              // Spread
+console .log( 'Array numeros1: ', numeros1 );
+console .log( 'Array numeros2: ', numeros2 );
+
+numeros1 .push( ...numeros2 );    // Agregamos valores como un Spread al Array 'numeros1'
+
+/* Resultado del Array 'numeros1' */
+console .group( 'Despues del Spread de numeros2 sobre numeros1' );
+console .log( 'Array numeros1: ', numeros1 );
+console .groupEnd();
