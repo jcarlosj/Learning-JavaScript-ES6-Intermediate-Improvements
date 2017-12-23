dccf89849473eb25d9f315f25f309d16bbef2494
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 17:42:16 -0500

    14 - Expresiones Regulares: Uso de la bandera 'y' (para búsqueda aditiva) y la propiedad lastIndex para ES6

--- expresiones-regulares/js/expresiones-regulares.js
+++ expresiones-regulares/js/expresiones-regulares.js
@@ -1,13 +1,22 @@
 /* Expresiones regulares en ES6 */
 
 /* De esta manera podemos verificar que nuestros navegadores soportan ES6 */
-var skicky;
+var cadena = 'Primera línea\nSegunda línea',
+    regex = /(\S+) línea\n?/y;      // 'y' es la bandera para hacer búsquedas pegajosas o aditivas en JavaScript
+                                    // Cualquier cosa que termine con ' línea\n'
 
-try {
-  RegExp( '', 'y' );
-  sticky = true;    // Si soporta ES6
-} catch (e) {
-    sticky = false;
-}
+console .group( 'Valores encontrados por la expresión regular' );
 
-alert( sticky );
+  var match1 = regex .exec( cadena );
+  console .log( 'match1 ', match1 );            //  Primera
+  console .log( '# ', regex .lastIndex );       //  En el caracter 14 termina la primera busqueda
+
+  var match2 = regex .exec( cadena );
+  console .log( 'match2 ', match2 );            //  Segunda
+  console .log( '# ', regex .lastIndex );       //  En el caracter 27 termina la segunda búsqueda
+
+  var match3 = regex .exec( cadena );
+  console .log( 'match3 ', match3 );            //  null
+  console .log( '# ', regex .lastIndex );       //  0 No hay mas aciertos
+
+console .groupEnd( '' );
