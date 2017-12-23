3fac95e249fadda1a5dd0d58adac31646516b336
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 12:53:54 -0500

    12 - Propagación o Spread: Spread entre Objects (Objetos)

--- spread/js/spread.js
+++ spread/js/spread.js
@@ -1,23 +1,21 @@
 /* Objetos de propagación o Spread (Expansión) en ES6 */
 
-let letras = [ 'c', 'd', 'e' ];
+/* Spread entre Objetos */
+let persona = {
+  nombre: 'Elisa',
+  apellido: 'Giraldo',
+  licencia: true
+};
 
-/* Agregando valores de forma tradicional */
-let cadena1 = [ 'a', 'b', 'f', 'g' ];   // Forma tradicional
+let automovil = {
+  ...persona,       // Spread de un objeto
+  marca: 'Citroen',
+  color: 'rojo',
+  placa: 'EMG-077',
+  anio: 2016
+};
 
-cadena1 .push( letras[ 0 ] );
-cadena1 .push( letras[ 1 ] );
-cadena1 .push( letras[ 2 ] );
-
-/* Agregando usando el Spread */
-let cadena2 = [ 'a', 'b', ...letras, 'f', 'g' ];
-
-console .group( 'Spread con Arrays' );
-console .log( "letras ... ", letras );
-  console .group( 'Tradicional' );
-    console .log( "cadena1 ... ", cadena1 );  // Tradicional
-  console .groupEnd();
-  console .group( 'Spread' );
-    console .log( "cadena2 ... ", cadena2 );  // Spread
-  console .groupEnd();
+console .group( 'Spread entre Objetos' )
+  console .log( 'Object: Persona ... ', persona );
+  console .log( 'Object: Automovil ... ', automovil );
 console .groupEnd();
