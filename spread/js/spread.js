6d6293522f1737f19334ec1bbdc479cdf80654ef
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 12:08:16 -0500

    12 - Propagación o Spread: Spread Objetos (parámetro de una función)

--- spread/js/spread.js
+++ spread/js/spread.js
@@ -1,14 +1,9 @@
 /* Objetos de propagación o Spread (Expansión) en ES6 */
 
-var numeros1 = [ 0, 1, 2 ],
-    numeros2 = [ 3, 4, 5 ];
+function contar( ...elementos ) {
+  console .log( 'Cantidad de elementos del Array: ', elementos.length );
+}
 
-console .log( 'Array numeros1: ', numeros1 );
-console .log( 'Array numeros2: ', numeros2 );
-
-numeros1 .push( ...numeros2 );    // Agregamos valores como un Spread al Array 'numeros1'
-
-/* Resultado del Array 'numeros1' */
-console .group( 'Despues del Spread de numeros2 sobre numeros1' );
-console .log( 'Array numeros1: ', numeros1 );
-console .groupEnd();
+contar();     // 0
+contar( 1, 3, 5, 7, );  // 4
+contar( 'Juana', true, 10, undefined, null, '', [], {} ); // 8
