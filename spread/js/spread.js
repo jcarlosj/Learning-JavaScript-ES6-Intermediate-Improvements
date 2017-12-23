83f00954605b9a9a631f6e7a4815a8a0d7107be4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 12:24:36 -0500

    12 - Propagación o Spread: Spread con funciones (Multiplicación)

--- spread/js/spread.js
+++ spread/js/spread.js
@@ -1,9 +1,24 @@
 /* Objetos de propagación o Spread (Expansión) en ES6 */
 
-function contar( ...elementos ) {
-  console .log( 'Cantidad de elementos del Array: ', elementos.length );
+function multiplicarPor( multiplicador, ...numeros ) {
+  // variables donde se almacenarán los resultados [ num1, num2, num3 ] .map
+  return numeros .map(
+    /* Función Anonima como parámetro */
+    function( elemento ) {
+      return multiplicador * elemento;
+    }
+  );   // map()
 }
 
-contar();     // 0
-contar( 1, 3, 5, 7, );  // 4
-contar( 'Juana', true, 10, undefined, null, '', [], {} ); // 8
+var salida = multiplicarPor( 2, 1, 2, 3 );
+
+console .group( '2 multiplica a: 1, 2, 3' );
+  console .log( salida );   // [ 2, 4 ,6 ]
+console .groupEnd();
+
+var numeros = [ 3, 4, 5 ],
+    salida = multiplicarPor( 2, ...numeros );
+
+console .group( '2 multiplica a: 3, 4, 5' );
+  console .log( salida );   // [ 6, 8 ,10 ]
+console .groupEnd();
