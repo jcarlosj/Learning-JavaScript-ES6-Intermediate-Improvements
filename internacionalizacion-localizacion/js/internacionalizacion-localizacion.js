fe482c61deb46b7132c04dce747ae5965f136dd9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 11:02:07 -0500

    11 - Internacionalización y Localización: Formato de monedas por zona

--- internacionalizacion-localizacion/js/internacionalizacion-localizacion.js
+++ internacionalizacion-localizacion/js/internacionalizacion-localizacion.js
@@ -1,17 +1,20 @@
 /* Internacionalización y Localizción en ES6 */
 
-/* Instancias de internacionalización para Formato de Números por zona */
-var l10nEN = new Intl .NumberFormat( 'en-US' ),
-    l10nDE = new Intl .NumberFormat( 'de-DE' );
+/* Instancias de internacionalización para Formato de Monedas por zona */
+var l10nUSD = new Intl .NumberFormat( 'en-US', { style: 'currency', currency: 'USD' } ),
+    l10nGBP = new Intl .NumberFormat( 'en-GB', { style: 'currency', currency: 'GBP' } ),
+    l10nEUR = new Intl .NumberFormat( 'de-DE', { style: 'currency', currency: 'EUR' } );
 
 console .group( 'Intl ' );
-  console .group( 'NumberFormat Method: Formateo de 123456.89 por zona: ' );
-    console .log( 'Convertir a formato Americano: ',l10nEN .format( 123456.89 ) );
-    console .log( 'Convertir a formato Europeo (Alemán): ',l10nDE .format( 123456.89 ) );
+  console .group( 'NumberFormat Method: Formateo de 100200300.40 por zona: ' );
+    console .log( 'Dólar Americano (USD): ',l10nUSD .format( 100200300.40 ) );
+    console .log( 'Libras esterlinas (GBP): ',l10nGBP .format( 100200300.40 ) );
+    console .log( 'Euros (EUR): ',l10nEUR .format( 100200300.40 ) );
   console .groupEnd();
   /* Relizamos una comparación de acuerdo a la internacionalización de cada idioma y reordenamiento */
-  console .group( 'Formato númerico de 123456.89 en: ' );
-    console .log( '1,234,567,89 está en formato Americano: ',l10nEN .format( 123456.89 ) === '1,234,567,89' );
-    console .log( '1.234.567.89 está en formato Europeo (Alemán): ',l10nDE .format( 123456.89 ) === '1.234.567.89' );
+  console .group( 'Formato moneda de 100200300.40 en: ' );
+    console .log( '$100,200,300.40 está en formato Dólar Americano (USD): ', l10nUSD .format( 100200300.40 ) === '$100,200,300.40' );
+    console .log( '£100,200,300.40 está en formato Libras esterlinas (GBP): ', l10nGBP .format( 100200300.40 ) === '£100,200,300.40' );
+    console .log( '100.200.300,40 € está en formato Euros (EUR): ', l10nEUR .format( 100200300.40 ) === '100.200.300,40 €' );
   console .groupEnd();
 console .groupEnd();
