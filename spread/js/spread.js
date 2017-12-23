d5a6c62e35a516613ffad0bf613b73300e0bb8b2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 11:50:28 -0500

    12 - Propagación o Spread: Spread con Arrays y llamada a funciones (Ejemplo 2)

--- spread/js/spread.js
+++ spread/js/spread.js
@@ -1,9 +1,8 @@
 /* Objetos de propagación o Spread (Expansión) en ES6 */
 
-const sumar = ( a, b ) => a + b;    // Arrow Function
-let nums = [ 3, 5 ];                // Array
+function saludar( nombre, apellido, edad ) {
+  alert( 'Hola ' + nombre + ' ' + apellido + ' que cumplas feliz tus ' + edad + ' años' );
+}
 
-console .group( 'Llamadas a funciones: Sumar() )' );
-  console .log( 'Tradicional ... ', sumar( nums[ 0 ], nums[ 1 ] ) );    // Forma tradicional
-  console .log( 'Spread ... ', sumar( ...nums ) );                            // Spread
-console .groupEnd();
+var persona = [ 'Laura', 'Fonseca', 29 ];           // Array
+saludar( ...persona );                              // Spread
