5b7ac375c5e6b2e69c120ba643f9018012e92239
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-23 10:51:57 -0500

    11 - Internacionalización y Localización: Formato de números por zona

--- internacionalizacion-localizacion/js/internacionalizacion-localizacion.js
+++ internacionalizacion-localizacion/js/internacionalizacion-localizacion.js
@@ -1,24 +1,17 @@
 /* Internacionalización y Localizción en ES6 */
 
-/* En alemán la letra (ä) se ordena junto a la letra (a). Sin embargo
-   en sueco la letra (ä) se ordenaría junto a la entra (z) */
-
-var listaLetras = [ 'ä', 'a', 'z' ];
-
-/* Creamos la instancia del Objeto de Internacionalización */
-var l10nDE = new Intl .Collator( 'de' ),      // Para el idioma alemán
-    l10nSV = new Intl .Collator( 'sv' );      // Para el idioma sueco
+/* Instancias de internacionalización para Formato de Números por zona */
+var l10nEN = new Intl .NumberFormat( 'en-US' ),
+    l10nDE = new Intl .NumberFormat( 'de-DE' );
 
 console .group( 'Intl ' );
-  /* Relizamos una comparación de acuerdo a la internacionalización de cada idioma y reordenamiento */
-  console .group( 'Collator Method: Comparación sistemática por zona' );
-    console .log( ' - Aleman: ', listaLetras .sort( l10nDE .compare ) );     // Ordena asi: [ 'a', 'ä', 'z' ]
-    console .log( ' - Sueco: ', listaLetras .sort( l10nSV .compare ) );      // Ordena asi: [ 'a', 'z', 'ä' ]
+  console .group( 'NumberFormat Method: Formateo de 123456.89 por zona: ' );
+    console .log( 'Convertir a formato Americano: ',l10nEN .format( 123456.89 ) );
+    console .log( 'Convertir a formato Europeo (Alemán): ',l10nDE .format( 123456.89 ) );
   console .groupEnd();
-
-  /* Usamos el método compare de la clase Intl */
-  console .group( 'Internacionalización: compare()' );
-    console .log( ' - Aleman: ', l10nDE .compare( 'ä', 'z' ) );  // -1 Orden correcto (primero va ä y luego z en Alemán )
-    console .log( ' - Sueco: ', l10nSV .compare( 'ä', 'z' ) );   //  1 Orden incorrecto (primero va z y luego ä en Sueco )
+  /* Relizamos una comparación de acuerdo a la internacionalización de cada idioma y reordenamiento */
+  console .group( 'Formato númerico de 123456.89 en: ' );
+    console .log( '1,234,567,89 está en formato Americano: ',l10nEN .format( 123456.89 ) === '1,234,567,89' );
+    console .log( '1.234.567.89 está en formato Europeo (Alemán): ',l10nDE .format( 123456.89 ) === '1.234.567.89' );
   console .groupEnd();
 console .groupEnd();
