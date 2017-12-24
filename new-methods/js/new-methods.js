8916d16ea7decc543d442070826db2b5c715a19d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 17:24:05 -0500

    15 - Metodos nuevos: EPSILON (Validando números decimales)

--- new-methods/js/new-methods.js
+++ new-methods/js/new-methods.js
@@ -1,8 +1,12 @@
 /* Nuevos métodos en ES6 */
 
-/* isSafeInteger */
+/* EPSILON
+ * JavaScript no es un lenguaje pensado en el procesamiento numérico por lo que
+ * puede dar algunos problemas haciendo calculos en los que se requieren Resultados
+ * exactos */
 
-console .group( 'isSafeInteger' );
-  console .log( '42 es un número seguro: ', Number .isSafeInteger( 42 ) );
-  console .log( '9007199254740992 es número seguro: ', Number .isSafeInteger( 9007199254740992 ) );   // Solo soporta hasta 9007199254740991
+console .group( 'EPSILON' );
+  console .log( '0.3 es igual a 0.3 ( con === ) ', 0.1 + 0.2 === 0.3 );    // False
+  console .log( '0.3 es igual a 0.3 ( abs() y EPSILON ) ', Math .abs( ( 0.1 + 0.2 ) - 0.3 ) < Number .EPSILON );   // True
+                                                                        // Alternativa a la línea anterior
 console .groupEnd();
