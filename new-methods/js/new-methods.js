2d9b746464363adf0bd22b08c4af1a6042028495
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 17:14:29 -0500

    15 - Metodos nuevos: isSafeInteger (Números seguros en JavaScript)

--- new-methods/js/new-methods.js
+++ new-methods/js/new-methods.js
@@ -1,16 +1,8 @@
 /* Nuevos métodos en ES6 */
 
-/* IsNaN */
+/* isSafeInteger */
 
-console .group( 'isNaN' );
-  console .log( '42 es un NaN: ', Number .isNaN( 42 ) );
-  console .log( 'NaN es un NaN: ', Number .isNaN( NaN ) );
-console .groupEnd();
-
-/* IsFinite */
-console .group( 'isFinite' );
-  console .log( 'Infinity es un valor Finito (positivo): ' , Number .isFinite( Infinity ) );
-  console .log( '-Inifinity es un valor Finito (negativo) ' , Number .isFinite( -Infinity ) );
-  console .log( 'NaN es un valor Finito ' , Number .isFinite( NaN ) );
-  console .log( '123 es un valor Finito ' , Number .isFinite( 123 ) );
+console .group( 'isSafeInteger' );
+  console .log( '42 es un número seguro: ', Number .isSafeInteger( 42 ) );
+  console .log( '9007199254740992 es número seguro: ', Number .isSafeInteger( 9007199254740992 ) );   // Solo soporta hasta 9007199254740991
 console .groupEnd();
