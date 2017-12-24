b66d94decce9e341aa5d640530caacc7963d65a8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 17:34:00 -0500

    15 - Metodos nuevos: sign (Valida el signo de un determinado número)

--- new-methods/js/new-methods.js
+++ new-methods/js/new-methods.js
@@ -1,10 +1,12 @@
 /* Nuevos métodos en ES6 */
 
-/* Trunc
- * Elimina los decimales (No redondea) */
+/* Sign
+ * Valida el signo (positivo, negativo) */
 
-console .group( 'Trunc' );
-  console .log( 'Elimina los decimales de 42.7 ... ', Math .trunc( 42.7 ) );     // 42
-  console .log( 'Elimina los decimales de 0.1 ... ', Math .trunc( 0.1 ) );       // 0
-  console .log( 'Elimina los decimales de -0.2 ... ', Math .trunc( -0.1 ) );     // -0
+console .group( 'Sign' );
+  console .log( 'El signo de 7 es ... ', Math .sign( 7 ) );       // 1
+  console .log( 'El signo de 0 es ... ', Math .sign( 0 ) );       // 0
+  console .log( 'El signo de -0 es ... ', Math .sign( -0 ) );     // -0
+  console .log( 'El signo de -7 es ... ', Math .sign( -7 ) );     // -1
+  console .log( 'El signo de NaN es ... ', Math .sign( NaN ) );   // NaN
 console .groupEnd();
