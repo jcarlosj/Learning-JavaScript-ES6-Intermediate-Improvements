ef6b874ccf0259f149c89c83e3916bbf6ac1d4f7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 18:43:35 -0500

    16 - Promises: Ejemplo 2

--- promises/js/promises.js
+++ promises/js/promises.js
@@ -1,39 +1,25 @@
 /* Promises en ES6 */
 
 /* CREACION DE PROMESAS */
-/* Promise con funciones anonimas tradicionales */
-let miPromesa1 = new Promise( function( resolve, reject ) {
-  setTimeout( function() {
-    resolve( 'Todo bien!' );
-  }, 1000 );
-  setTimeout( function() {
-    reject( 'Todo mal :(' );
-  }, 500 );                       // Se ejecuta primero por que esta a segundo y medio de ejecución respecto a la anterior
-});
-/* Promise con Arrow Functions (Fat Arrows) */
-let miPromesa2 = new Promise( ( resolve, reject ) => {
-  setTimeout( () => {
-    resolve( 'Todo bien!' );
-  }, 1000 );                      // Se ejecuta primero por que esta a segundo de ejecución respecto a la siguiente
-  setTimeout( () => {
-    reject( 'Todo mal :(' );
-  }, 1500 );
+let miPromesa = new Promise( function( todoBien, todoMal ) {
+  let resultado = false;
+
+  if( resultado ) {
+    todoBien( 'Listo Medellín! ');
+  }
+  else {
+    todoMal( 'Algo falló parce' );
+  }
 });
 
 /* DISPARADOR O EJECUCIÓN DE PROMESAS */
-/* Promesa con funciones tradicionales */
 console .group( 'Promises' );
 
-miPromesa1 .then( function ( res ) {
-  console .log( 'Promesa 1: ', res );
-}, function( err ) {
-  console .log( 'Promesa 1: ERROR ', err );
-});
-/* Promesa con Arrow Functions (Fat Arrow) */
-miPromesa2 .then( ( res ) => {
-  console .log( 'Promesa 2: ', res );
-}, ( err ) => {
-  console .log( 'Promesa 2: ERROR ', err );
+miPromesa .then( function ( resultado ) {
+  console .log( 'El resultado es: ', resultado );
+})
+.catch( function( resultado ) {
+    console .log( 'El resultado es: ', resultado );
 });
 
 console .groupEnd();
