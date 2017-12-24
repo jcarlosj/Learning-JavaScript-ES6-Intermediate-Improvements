d8f832f7482170a1f1f4556daac85588f5396122
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 16:50:30 -0500

    15 - Metodos nuevos: StartsWith, EndsWith, Includes (Encontrar subcadenas en una cadena)

--- new-methods/js/new-methods.js
+++ new-methods/js/new-methods.js
@@ -1,5 +1,11 @@
 /* Nuevos métodos en ES6 */
 
-/* Repeat */
-console .log( '*' .repeat( 4 * 10 ) );
-console .log( 'Hola! ' .repeat( 3 ) );
+/* StartWith, EndsWith, Includes */
+var cadena = 'Hola carola cara de bola';
+
+/* Resultados aplicando estos métodos */
+console .log( cadena .startsWith( 'ola', 1 ) );     // Encuentra la palabra 'ola' apartir del caracter 1
+console .log( cadena .endsWith( 'ola' ) );          // Finaliza con 'ola'
+console .log( cadena .includes( 'ola' ) );          // Incluye 'ola'
+console .log( cadena .includes( 'ola', 1 ) );       // Incluye 'ola' en la posición 1
+console .log( cadena .includes( 'ola', 2 ) );       // Incluye 'ola' en la posición 2
