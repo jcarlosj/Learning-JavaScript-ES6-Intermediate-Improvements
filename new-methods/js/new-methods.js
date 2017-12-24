eb8ac321953c6b9b40414f23bd4be5b7d5e29f25
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 17:30:08 -0500

    15 - Metodos nuevos: trunc (Elimina los decimales)

--- new-methods/js/new-methods.js
+++ new-methods/js/new-methods.js
@@ -1,12 +1,10 @@
 /* Nuevos métodos en ES6 */
 
-/* EPSILON
- * JavaScript no es un lenguaje pensado en el procesamiento numérico por lo que
- * puede dar algunos problemas haciendo calculos en los que se requieren Resultados
- * exactos */
+/* Trunc
+ * Elimina los decimales (No redondea) */
 
-console .group( 'EPSILON' );
-  console .log( '0.3 es igual a 0.3 ( con === ) ', 0.1 + 0.2 === 0.3 );    // False
-  console .log( '0.3 es igual a 0.3 ( abs() y EPSILON ) ', Math .abs( ( 0.1 + 0.2 ) - 0.3 ) < Number .EPSILON );   // True
-                                                                        // Alternativa a la línea anterior
+console .group( 'Trunc' );
+  console .log( 'Elimina los decimales de 42.7 ... ', Math .trunc( 42.7 ) );     // 42
+  console .log( 'Elimina los decimales de 0.1 ... ', Math .trunc( 0.1 ) );       // 0
+  console .log( 'Elimina los decimales de -0.2 ... ', Math .trunc( -0.1 ) );     // -0
 console .groupEnd();
