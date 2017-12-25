474f802c7ad30f2c2ac0f9033d4e0dfa5f696b49
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-24 19:13:00 -0500

    16 - Promises: Ejemplo 5 (Anidando Todas las promesas al tiempo)

--- promises/js/promises.js
+++ promises/js/promises.js
@@ -1,24 +1,29 @@
 /* Promises en ES6 */
 
 /* CREACION DE PROMESAS */
-let primeraPromesa = new Promise( ( resolve, reject ) => {
-  setTimeout( () => {
-    resolve( 'Primera Promesa - Todo Bien' );
-  }, 1000 );                                      // 1 segundo
-});
-
-let segundaPromesa = new Promise( ( resolve, reject ) => {
-  setTimeout( () => {
-    resolve( 'Segunda Promesa - Todo Bien' );
-  }, 500 );                                      // 0.5 segundos
-});
-
+let primeraPromesa = function() {
+  return new Promise( function( resolve, reject ) {
+    resolve( 'Primera promesa lista! ' );
+  });
+};
+let segundaPromesa = function() {
+  return new Promise( function( resolve, reject ) {
+    resolve( 'Segunda promesa lista! ' );
+  });
+};
+let terceraPromesa = function() {
+  return new Promise( function( resolve, reject ) {
+    resolve( 'Tercera promesa lista! ' );
+  });
+};
 /* Encadenamos las Promesas */
-Promise .all([ primeraPromesa, segundaPromesa ])
-  .then( data => {
-    console .log( data );
+Promise .all([ primeraPromesa(), segundaPromesa(), terceraPromesa() ])
+  .then( respuestas => {
+    respuestas .forEach( function( mensaje ) {
+      console .log( mensaje );
+    });
   })
   .catch( err => console .log( 'ERROR ', err ) );
 
 /* NOTA: Si invertimos los tiempos los datos que se imprimirán serán los de la última
- *       que es la que prevalece al ejecutarse bien la primera y bien la segunda */  
+ *       que es la que prevalece al ejecutarse bien la primera y bien la segunda */
